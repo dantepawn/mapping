6fcf4c98e3841fb80f0c9c9b776789243f8d8d4a
Daniel Pavlicek <dantepawn@users.noreply.github.com>
2021-11-01 15:45:54 +0100

    new places
    function created for parsing
    HereV7 geocoder used
    best places for autumn for hikes close to rome

---
+++ location_txt.py
@@ -0,0 +1,154 @@
+colazione = '''L’Antica Ciriola (Quadraro)
+Canova Tadolini, via del babuino 150 () 
+Chiostro del Bramante, Arco della Pace ()
+Il Gianfornaio (Ponte Milvio, Parioli, Prati, Testaccio, Viale Libia, Eur)
+Via Gregorio VII()
+Biffi (Garbatella)
+Faro (Via Piave, 55)
+Regoli (Via dello Statuto, 60) Maritozzi
+Cristalli di Zucchero (Monteverde)
+Le Levain (Trastevere) francese
+Gruè (viale Regina Margherita, 95)
+Mizzica (Piazza Bologna)
+Babington ,Piazza di Spagna,()
+Abate (Prima Porta)
+Bakery house (Trieste, Ponte Milvio, Eur)
+Rosti (Pigneto)
+Vanni (Prati)
+Antonini Roma (Piazza Mazzini)
+Romoli (Viale Eritrea) krantz
+La buvette (Via Vittoria)
+Favole di pane, via Tiburtina,() cornetto con la crema
+Il dondolo (Città giardino)
+Hiromi Cake (Prati, Salario)
+Via del serafico 167(Trieste)
+Marigold ,Via Giovanni da Empoli 37, Roma,
+Hotel Eden (Via Ludovisi) brunch 90€ ma vista mozzafiato 
+Bompiani (Via delle Sette Chiese) burro
+Roscioli (Campo de’ fiori) maritozzo anche in versione salata
+Tornatora Tuscolana (Quadraro) siciliano
+Il Tornatora (Prati fiscali)
+Vintro bar & bites (Viale Jonio)
+Maat Bakery & Bistrot ,Via Sestio Calvino 20/2 ()
+Belle Hélène, Via Giuseppe Garibaldi 12 ,Tarquinia())
+Pasticceria D'Antoni, Via Dino Penazzato (Collatina) ci ha lavorato un ascoltatore
+Corsetti (Pigneto) tramezzini
+Wani (Ostiense) vegan
+Faggiani (Prati)
+Caffè Tornatora Roma (Via Igea) danese
+Il Maritozzaro (Porta Portese)
+La gatta ghiotta (Manziana)
+Gran Caffè Dei Colli , Viale dei colli portuensi 518,(Monteverde)
+Krapfen Paglia (Ostia)
+Bar Casoria ,Viale Antonio Ciamarra 59(Romanina) aragostine caffè 0.70€
+Benaco (Trieste) aragostine nutella
+Casa Manfredi (Viale Aventino)
+Duranti ( Via val di cogne)
+Taki Matcha Caffè Via Marianna Dionigi 62
+Pasticceria Valentini (Conca d’oro) cornetto marmellata di visciole
+Pasticceria L’arte del dolce (Velletri) diplomatico
+Bar Tornatora (Via Oderisi da Gubbio)
+Bar Mazzaroni (Magliana)
+Pasticceria Cecere (Via Pian Due Torri, 20)
+Homebaked , Porta Cavalleggeri()) brunch
+Via di Monserrato 46(centro storico)
+Il Pangocciolaio (Ostiense)
+Le Carré Français (Via Vittoria Colonna)
+Charlotte (San Giovanni)
+Bar Pasticceria Pino (San Cesareo)
+Ruschena (Lungotevere)
+Castroni (Via Cola de Rienzo)
+Sant'Eustachio Il Caffè
+'Na Tazzulella 'e cafè, Viale dell'Aeronautica 57,(Eur)
+Bar degli Estensi()
+Gran Caffè Hawaii, Via di Tor Pignattara 43 ()'''
+
+
+aperitivo = '''Mille 13, via dei mille 13
+Fraschette coppiette, quartino 
+Rosti
+Woods Malatesta
+Fassangue
+Deja vu, Centocelle 
+Enofficina, via trionfale 50 
+Rude, Centocelle
+Rosso, Aventino
+Ombralonga
+Shat mat, 
+Hop corner
+Hop & Pork, 
+l’intruso, 
+Portale 21, 
+ngoccetto, 
+Salotto 42,
+Il  baretto  di 40, Duilio 1
+Zerozero100
+Mad hop
+
+Santo bevitore
+
+Soul train
+
+Di Renzo, pantheon 
+
+Freni e frizioni
+
+Momart café
+
+Romakinho
+
+Magno bevo e sto 
+
+Al solito posto
+
+Chorus
+
+Doppio zero, 
+
+Bar monaco
+
+Il vinaietto
+
+Tram Depot
+
+Roscioli
+
+Beppe e i suoi formaggi
+
+Saltinbocca, 
+
+Bestia mora, 
+
+Salumeria magrelli
+
+Il vigneto
+
+Celestino 
+
+Jungle juice
+'''
+
+autunno = '''Soriano nel cimino, la faggeta
+Villa Pamphili
+Villa Borghese
+Lungotevere
+Via Sacra
+Benevento
+Anversa degli abruzzi, gole del sagittario
+Sotto il monte Tarino, campo staffi
+Castagneti di Lago di Vico
+Ronciglione
+Monte Livata a Subiaco
+Il giardino degli aranci
+Prati di Tivo
+Monti della laga, bosco dei faggi torti ,Abruzzo
+Bomarzo
+Villa Celimontana
+Forca d'acero , Pescasseroli
+Le foreste casentinesi 
+Lago del Salto
+Il bosco di Manziana
+l'orto botanico ,via della Lungara 
+Monte Tuscolo
+Parco Botanico di San Liberato
+'''
